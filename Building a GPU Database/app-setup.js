71cb62519c00dedc481387c1f50cf72ca9931fa6
lalithvenkat <37024153+lalithvenkat@users.noreply.github.com>
2021-08-23 00:05:25 +0100

    Add files via upload

---
+++ Building a GPU Database/app-setup.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAiCWVs1ydu-sTybk09RJbjnxFRmzx7Dyk",
+    authDomain: "gpus-fdbc3.firebaseapp.com",
+    projectId: "gpus-fdbc3",
+    storageBucket: "gpus-fdbc3.appspot.com",
+    messagingSenderId: "478773813585",
+    appId: "1:478773813585:web:1e4848fd2bb59bc6d83370",
+    measurementId: "G-CXCB6D3JYM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+

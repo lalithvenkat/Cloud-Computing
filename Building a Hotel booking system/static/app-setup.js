e24504cb53e6f04770f42631f30ac52018d7e0d2
lalithvenkat <37024153+lalithvenkat@users.noreply.github.com>
2021-08-25 00:06:29 +0100

    Add files via upload

---
+++ Building a Hotel booking system/static/app-setup.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAmSP7YSIy_H-0_Uot__9BIBdVsjqu_yz8",
+    authDomain: "rooms-312813.firebaseapp.com",
+    projectId: "rooms-312813",
+    storageBucket: "rooms-312813.appspot.com",
+    messagingSenderId: "270666680852",
+    appId: "1:270666680852:web:dfa27910b5d7abe1eab501"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

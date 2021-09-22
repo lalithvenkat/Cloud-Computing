33923bc03349c30624e6246a67c7f1d63081a62a
lalithvenkat <37024153+lalithvenkat@users.noreply.github.com>
2021-09-23 00:28:32 +0100

    Add files via upload

---
+++ Creating a Dropbox Application/static/app-setup.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAcUvBfMbEYtnkdHkDK_XStI-_GQ1FXNnQ",
+    authDomain: "dropbox-313912.firebaseapp.com",
+    projectId: "dropbox-313912",
+    storageBucket: "dropbox-313912.appspot.com",
+    messagingSenderId: "644636270114",
+    appId: "1:644636270114:web:fe170c9d609c36f54af47b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
